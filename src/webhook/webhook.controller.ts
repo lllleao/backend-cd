6a9e96636b43a4000dcc071d6cdb978583458fe2
leao <lucasleaolima@gmail.com>
2025-09-16 20:34:17 -0300

    feat: route pix

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,9 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common'
 import { Request, Response } from 'express'
-import { TLSSocket } from 'tls'
-import * as fs from 'fs'
-import * as path from 'path'
 import { WebHookApiPixService } from './webhoos.service'
 
 @Controller('webhook')
@@ -25,18 +22,12 @@ export class WebHookApiPixController {
 
     @Post('pix')
     handleWebhookPix(@Req() req: Request, @Res() res: Response) {
-        const socket = res.socket as TLSSocket
-
-        if (socket.authorized) {
-            const body = req.body
-            const filePath = path.join(__dirname, '..', '..', 'data.json')
-
-            fs.appendFile(filePath, JSON.stringify(body) + '\n', (err) => {
-                if (err) {
-                    console.error('Erro ao salvar webhook:', err)
-                }
-                res.status(200).end()
-            })
+        console.log('chegou na rota pix')
+        const clientVerify = req.headers['ssl_client_verify']
+        console.log('clientVerify Pix: ', clientVerify)
+        // const socket = res.socket as TLSSocket
+        if (clientVerify === 'SUCCESS') {
+            res.status(200).end()
         } else {
             res.status(401).end()
         }
